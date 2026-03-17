1ef58dbd93f3e8fc6bd4b42dcc4ede0462dc49cc
pongsathon <pongsathon.m@swiftdynamics.co.th>
2026-03-17 14:40:26 +0700

    test

--- backend/src/rooms/rooms.gateway.ts
+++ backend/src/rooms/rooms.gateway.ts
@@ -199,7 +199,6 @@ export class RoomsGateway {
             description?: string;
             link?: string;
           };
-          console.log('=== DEBUG create-card ===');
 
           if (!createCardPayload.roomId) return;
 
